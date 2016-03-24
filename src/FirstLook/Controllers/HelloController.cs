0fefb1456c1bd8601597985cdd2856636cbc9d02
derekscode <derekscode@github.com>
2016-03-23 17:21:25 -0700

    initial commit

---
+++ src/FirstLook/Controllers/HelloController.cs
@@ -0,0 +1,29 @@
+﻿using FirstLookLibrary;
+using Microsoft.AspNet.Mvc;
+using System.Reflection;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FirstLook.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: /<controller>/
+        public string Index()
+        {
+
+            var greeter = new Greeter();
+
+            //var greeting = greeter.
+
+
+#if dnx451
+            return Assembly.GetExecutingAssembly().FullName;
+#endif
+#if dnxcore50
+            return "hello";
+#endif
+            return null;
+        }
+    }
+}
---
+++ src/FirstLookLibrary/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FirstLookLibrary")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Outerwall Inc")]
+[assembly: AssemblyProduct("FirstLookLibrary")]
+[assembly: AssemblyCopyright("Copyright © Outerwall Inc 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ea84c692-4314-46b2-b683-9501b7be85c7")]
